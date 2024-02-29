31e0cd6f62cda6d7aea410d7b448c9a6bd8ca51a
khaled-adarani <khaledadrani@gmail.com>
2024-02-29 17:23:21 +0100

    feature: add prometheus fastapi metric endpoint

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from pyctuator.pyctuator import Pyctuator
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from source.acetl_web.apis import data_fetch_router
 from source.acetl_web.app_config import AppConfig
@@ -21,19 +21,12 @@ async def lifespan(app: FastAPI):
 web_app = FastAPI(lifespan=lifespan)
 web_app.container = DependencyContainer()
 
-web_app.add_middleware(MonitoringMiddleware)
+# web_app.add_middleware(MonitoringMiddleware)
 
 app_config = AppConfig()
 
 web_app.include_router(data_fetch_router, tags=["LLM GENERATION API"])
-
-Pyctuator(
-    web_app,
-    "FastAPI Pyctuator",
-    app_url="http://localhost:8000",
-    pyctuator_endpoint_url="http://localhost:8000/pyctuator",
-    registration_url="http://localhost:8080/instances"
-)
+Instrumentator().instrument(web_app).expose(web_app)
 
 # Run the FastAPI application with uvicorn
 if __name__ == "__main__":
