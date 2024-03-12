78f1e435d0031482ccbb65e85fd83520afb2c3d8
khaledadrani <khaledadrani@gmail.com>
2024-03-12 17:53:36 +0100

    feature: read_page function, unit tests in progress

--- source/etl_pdf/pdf_extractor_pipeline.py
+++ source/etl_pdf/pdf_extractor_pipeline.py
@@ -1,27 +1,32 @@
-from typing import Optional
+from typing import List
+from typing import Optional, Generator
 
 import fitz
 
 
-class PdfExtractorPipeline:
-    def __init__(self, file_path: str):
-        self.file_path = file_path
-        self.document: Optional[fitz.Document] = None
-        self.page = None
-        self.text = None
+def read_pdf(file_path: str,
+             start_index: Optional[int] = 0,
+             page_count: Optional[int] = 1) -> Generator[str, None, None]:
+    """
+    Read text from a PDF file.
 
-    def extract(self):
-        self.document = fitz.open(self.file_path)  # should with better
+    Args:
+        file_path (str): The path to the PDF file.
+        start_index (Optional[int]): The index of the starting page (default is 0).
+        page_count (Optional[int]): The number of pages to read (default is 1).
 
-    def transform(self):
-        self.page = self.document.load_page(1)
+    Yields:
+        str: Text content of each page in the specified range.
+    """
+    with fitz.open(file_path) as document:
+        for index in range(start_index, (start_index + page_count)):
+            page = document[index]
+            yield page.get_text()
 
-    def load(self):
-        self.text = str(self.page)
-        self.document.close()
 
-    def __call__(self):
-        self.extract()
-        self.transform()
-        self.load()
-        return self.text
+if __name__ == "__main__":
+    file_path = "D:\\Books\\numpy reference.pdf"
+    text: List[str] = []
+    ls = list(read_pdf(file_path, 0, 5))
+    for l in ls:
+        print(len(l))
