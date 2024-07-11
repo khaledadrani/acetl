8127e40c1b68107335506966265a44511f821e81
Khaled Adrani <khaled.adrani@infor.com>
2024-07-11 13:49:17 +0100

    feature: implement Minio connector initial working version

--- source/common/gateways/file_systems/minio_connector.py
+++ source/common/gateways/file_systems/minio_connector.py
@@ -1,16 +1,35 @@
+import io
+
 from minio import Minio
 
 from source.common.gateways.file_systems.base import FileSystemInterface
 
 
-class MinIOConnector(FileSystemInterface):
-    def __init__(self): #TODO config class
-        self.client = Minio(
-            access_key="admin",
-            secret_key="admin_admin",
-            endpoint="localhost:9000",
-            secure=False
-        )
+class MinioFileSystem(FileSystemInterface):
+    def __init__(self, endpoint: str, access_key: str, secret_key: str, bucket_name: str):
+        self.client = Minio(endpoint, access_key=access_key, secret_key=secret_key, secure=False)
+        self.bucket_name = bucket_name
+        if not self.client.bucket_exists(bucket_name):
+            self.client.make_bucket(bucket_name)
 
-        print(self.client .list_buckets())
+    def store(self, key, data, content_type: str = 'application/octet-stream'):
+        if isinstance(data, str):
+            data = io.BytesIO(data.encode())
+        elif isinstance(data, bytes):
+            data = io.BytesIO(data)
+        self.client.put_object(self.bucket_name, key, data, len(data.getvalue()), content_type=content_type)
 
+    def get(self, key, mode: str = 'rb'):
+        try:
+            response = self.client.get_object(self.bucket_name, key)
+            return response.read()
+        except S3Error as e:
+            if e.code == 'NoSuchKey':
+                return None
+            else:
+                raise
+
+    def get_list(self, prefix: str = '', recursive: bool = False) -> Generator[str, None, None]:
+        objects = self.client.list_objects(self.bucket_name, prefix=prefix, recursive=recursive)
+        for obj in objects:
+            yield obj.object_name
